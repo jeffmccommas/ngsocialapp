c170b65c09b54c8aaca41b4f6030a39da1fb8070
Jeffrey McCommas <jmccommas@Jeffreys-MacBook-Pro.local>
2015-10-01 07:35:31 -0400

    First commit

---
+++ app/app.js
@@ -0,0 +1,38 @@
+/**
+ * Created by jmccommas on 9/30/15.
+ */
+
+
+
+
+var ngsocial = angular.module('ngsocial', [
+	'ngRoute',
+	'ngFacebook',
+	'ngsocial.facebook'
+]);
+
+ngsocial.config(function($routeProvider) {
+	$routeProvider
+		.when('/facebook', {
+			templateUrl: 'facebook/facebook.html',
+			controller: 'facebookController'
+		})
+		when('/about', {
+			templateUrl: 'about/about.html',
+			controller: 'ShowOrdersController'
+		})
+		.otherwise({
+			redirectTo: '/facebook'
+		});
+});
+
+
+
+
+
+
+
+
+
+
+
---
+++ facebook/facebook.js
@@ -0,0 +1,3 @@
+/**
+ * Created by jmccommas on 9/30/15.
+ */
